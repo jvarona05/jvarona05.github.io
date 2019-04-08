5541770701ac453341b5851e37133667256e1e70
José <jose.varona05@gmail.com>
2019-04-08 18:18:26 +0200

    update contact section and in testimonal section add links to linkedin per each person

--- js/scripts.js
+++ js/scripts.js
@@ -359,7 +359,7 @@ $(function () {
 */
 
 function initMap() {
-	var myLatlng = new google.maps.LatLng(40.773328,-73.960088); // <- Your latitude and longitude
+	var myLatlng = new google.maps.LatLng(40.3950986,-3.7375836); // <- Your latitude and longitude
 	var styles = [
 	{
 		"featureType": "water",
